0100fc4766228bd774629f510e2beb77fb0ba540
Hyesun Choi <chocochic85@gmail.com>
2019-04-02 22:42:49 +0900

    Create drawRectangle.swift

---
+++ Algorithm/programmers/drawRectangle.swift
@@ -0,0 +1,16 @@
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+if a <= 1000 && b <= 1000 {
+    for i in 0..<b {
+        for j in 0..<a {
+            print("*", separator: "", terminator: "")
+        }
+        print("")
+    }
+}else{
+    print("invalid")
+}
